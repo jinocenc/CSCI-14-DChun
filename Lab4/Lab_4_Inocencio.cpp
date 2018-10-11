faa02dcbb4b2d1f042965ba38d70a717b631bfb0
unknown <jerold.inocencio@gmail.com>
2018-10-10 23:19:12 -0700

    Added Lab 4 and doc

---
+++ Lab4/Lab_4_Inocencio.cpp
@@ -0,0 +1,34 @@
+/*
+CSCI 14 | Lab 3 | Jerold Inocencio
+This Lab exercise demonstrates the use of the 
+“For Loop”, a standard loop in all programming languages. 
+*/
+
+#include <iostream>
+using namespace std;
+
+int     main(void)
+{
+    int     N;
+    int     sum;
+
+    sum = 0;
+    cout << "Please input a number of time to run the For Loop: ";
+    cin >> N;
+
+    for (int n = 1; n <= N; n++)
+    {
+        sum += n;
+        cout << n;
+        if (n < N)
+        {
+            cout << " + ";
+        }
+        else 
+        {
+            cout << " = " << sum << endl;
+        }
+    }
+    cout << "You ran the loop " << N << " times.";
+    return 0;
+}
